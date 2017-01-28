986b4e15270ea72f55c2a6a89b9035412e26bd9e
Ognjen Vulic <ognjen.vulic@engagis.com>
2017-01-28 23:20:43 +0100

    More Styling

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -2,10 +2,22 @@
  * Created by OgnjenVulic on 1/16/2017.
  */
 var React = require('react');
+var {Link} = require('react-router');
 
 var Examples = (props)=>{
         return (
-            <h3>Examples Component !!</h3>
+            <div>
+                <h1 className="text-center">Examples</h1>
+                <p>Here are few examples:</p>
+                <ol>
+                    <li>
+                        <Link to="/?location=NoviSad">Novi Sad</Link>
+                    </li>
+                    <li>
+                        <Link to="/?location=New York">New York</Link>
+                    </li>
+                </ol>
+            </div>
         );
     };
 
